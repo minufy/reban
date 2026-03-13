5534d2a9c46aa09ebb64d6ee2f4987be66e2990a
minu <alsdn638@gmail.com>
2026-03-13 16:54:23 +0900

    2

--- stuff/utils.lua
+++ stuff/utils.lua
@@ -80,18 +80,18 @@ end
 Audio = {}
 function NewAudio(name, volume)
     volume = volume or 0.5
-    local sound = {
+    local audio = {
         source = love.audio.newSource("assets/audio/"..name..".ogg", "static"),
         volume = volume
     }
-    function sound:play(v)
+    function audio:play(v)
         v = v or self.volume
         self.source:setVolume(v)
         self.source:stop()
         self.source:play()
     end
-    Audio[name] = sound
-    return sound
+    Audio[name] = audio
+    return audio
 end
 
 function RoundS(x, r, ofs)
