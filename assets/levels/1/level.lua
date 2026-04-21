23fc7319b9c78f6ac49b99871211b8477e0924b5
minufy <seomeenu@proton.me>
2026-04-21 19:52:56 +0900

    add object rotation

--- assets/levels/1/level.lua
+++ assets/levels/1/level.lua
@@ -1,7 +1,1 @@
-return {
-    ["objects"] = {
-        ["0x01969cf00fc0"] = {["y"] = 0, ["type"] = "player", ["x"] = 16}
-    },
-    ["tiles"] = {["0,0"] = "tile"},
-    ["img_objects"] = {}
-}
+return {["tiles"]={["0,0"]="tile"},["img_objects"]={},["objects"]={["0x01969cf00fc0"]={["x"]=16,["type"]="player",["y"]=0}}}
